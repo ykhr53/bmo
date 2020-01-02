66cd42c6d720b84838167fa8457f821a8f7e9f30
yukihira <yukihira.lab@gmail.com>
2020-01-02 15:51:27 +0000

    add health check path

--- bmo.go
+++ bmo.go
@@ -124,6 +124,9 @@ func main() {
 	bmo := NewBMO()
 	mux := http.NewServeMux()
 	mux.Handle("/events-endpoint", bmo)
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
 	log.Fatal(http.ListenAndServe(":3000", mux))
 }
 
